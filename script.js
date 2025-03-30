136b0f6bfcbbacbc7651d54aec96030c1846b5dd
barakameek <139329662+barakameek@users.noreply.github.com>
2025-03-30 23:41:38 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// script.js - FINAL COMPLETE VERSION (v. Card Concept Expanded MVP)
 
-// --- Global State ---
 let currentElementIndex = 0;
 let userScores = { A: 5, I: 5, S: 5, P: 5, C: 5, R: 5 }; // Use single letter keys
 let userAnswers = {}; // Stores answers keyed by FULL element names during questionnaire
