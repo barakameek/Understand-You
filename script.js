027fd84cc2db2b2ac7f37e05c54f3f613cce24b1
barakameek <139329662+barakameek@users.noreply.github.com>
2025-03-31 00:09:20 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// --- Global State ---
 let currentElementIndex = 0;
 let userScores = { A: 5, I: 5, S: 5, P: 5, C: 5, R: 5 }; // Use single letter keys
 let userAnswers = {}; // Stores answers keyed by FULL element names during questionnaire
