a45ddaa64925d9ef2832070f00e846de0293731d
barakameek <139329662+barakameek@users.noreply.github.com>
2025-04-21 16:53:37 +1000

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ import * as State from './js/state.js';
 import * as GameLogic from './js/gameLogic.js';
 import * as Utils from './js/utils.js';
 import * as Config from './js/config.js';
-import { onboardingTasks, elementKeyToFullName } from './data.js'; // Import onboarding tasks & element map
+import { elementNames, concepts } from './data.js';
+//import { onboardingTasks, elementKeyToFullName } from './data.js'; // Import onboarding tasks & element map
 
 console.log("main.js loading... (Enhanced v4.1 + Drawer - Fixed v2)");
 
